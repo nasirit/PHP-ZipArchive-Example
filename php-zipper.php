fb0593e2756aca2509021109c945a5f62940c240
nasirit <33125927+nasirit@users.noreply.github.com>
2019-11-23 12:52:07 +0600

    Create php-zipper.php

---
+++ php-zipper.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>PHP ZipArchive Example</title>
+</head>
+
+<body>
+<center>
+	<form action="" method="post">
+		zip file Name: <br>
+		<input name="zipfile" value="test.zip"><br><br>
+		Location: <br>
+		<input name="location" value="location" ><br><br>
+		
+		<input type="submit" name="do" value="zip" > | 
+		<input type="submit" name="do" value="extract" >
+	</form>
+
+<?php 
+
+if(!empty($_REQUEST['do'])){
+	if($_REQUEST['do'] == 'zip'){
+		// Coded by NASIR IT (www.nasirit.com)
+
+		echo "<h3>Adding files...</h3>";
+		$zipfile = "$_REQUEST[zipfile]";
+		$location = "$_REQUEST[location]";
+		
+		$zip = new ZipArchive;
+		$zip->open($zipfile, ZipArchive::CREATE);
+		if(is_file($location)) $zip->addFile($location);
+		elseif(is_dir($location))
+		for($i=0; $i<100; $i++){ // highest folder depth
+		    if($location != '.' OR $location != '..')
+		    foreach (glob("$location/*") as $file){
+		        if(is_file($file)) $zip->addFile($file);
+		        else $location = $file;
+		    }
+		}
+		$zip->close();
+		
+		echo "<h2>Done</h2>";
+
+	}elseif($_REQUEST['do'] == 'extract'){
+		
+		echo "<h3>Extracting files...</h3>";
+		$zipfile = "$_REQUEST[zipfile]";
+		$location = "$_REQUEST[location]";
+		
+		$zip = new ZipArchive();
+		$zip->open($zipfile);
+		$zip->extractTo($location);
+		$zip->close();
+		
+		echo "<h2>Done</h2>";
+	}
+
+} 
+
+?>
+</center>
+</body>
+
+</html>
